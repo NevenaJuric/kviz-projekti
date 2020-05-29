bdb25801fdfb17e7e1fc1d67a4f7c6f15dea4901
NevenaJuric <nevenajuric96@gmail.com>
2020-05-29 12:53:47 +0200

    fixed routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NavigationComponent } from './components/navigation/navigation.component';
 
 
 const routes: Routes = [];
