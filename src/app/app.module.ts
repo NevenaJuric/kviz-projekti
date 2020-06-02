bf69833cd064c94bdb5203a703719009126fbdce
NevenaJuric <nevenajuric96@gmail.com>
2020-06-02 15:50:36 +0200

    delete home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { SlidebarMenuComponent } from './components/slidebar-menu/slidebar-menu.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    SlidebarMenuComponent,
-    HomeComponent
+    SlidebarMenuComponent
   ],
   imports: [
     BrowserModule,
